74a720d0dd0098457312010b21eeb5cc7e0fbfe4
MKS-students <mks.students@hackreactor.com>
2016-05-20 15:43:23 -0700

    [add] .gitignore and server.js files added. server/app.js modified

--- server/app.js
+++ server/app.js
@@ -1,5 +1,19 @@
 var express = require('express');
 var mongoose = require('mongoose');
+var app = express();
 
+module.exports = app;
+
+// update this later
 mongoose.connect('mongodb://localhost/myappdatabase')
 
+app.configure(function(){
+  app.use(express.bodyParser());
+});
+
+
+
+
+
+app.listen(3000);
+console.log('listening on local 3000')
