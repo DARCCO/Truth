966c91e4efbe959d70e31419c8447860f20ca4d8
austinsefton <austin.sefton@gmail.com>
2016-05-23 19:12:25 -0700

    [Add] [Progress] added config file to set a secret for user session. Added config file to .gitignore. edited authentication.js to issue a JWT when logged in

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,4 +1,14 @@
-const User = require('../models/user')
+const jwt = require('jwt-simple');
+const User = require('../models/user');
+const config = require('../config');
+
+function tokenForUser(user) {
+  // sub is a convention, jwt tokens have sub, short for subject
+  // subject of token is specific user
+  // iat is another convention: issued at time
+  const timestamp = new Date().getTime();
+  return jwt.encode({ sub: user.id, iat: timestamp }, config.secret)
+}
 
 exports.signup = function(req, res, next) {
   const email = req.body.email;
@@ -32,7 +42,7 @@ exports.signup = function(req, res, next) {
 
   	  // respond to request indicating user was created
 
-  	res.json(user);
+  	res.json({ token: tokenForUser(user) });
 
 
   })
